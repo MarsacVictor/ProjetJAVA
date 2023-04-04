e6ee160644a1273312a4a17a1a8ad65844f97be6
ccadu2022 <ccadu2022@139SE13-HXT14G2.ad.campus-eni.fr>
2023-04-04 09:52:13 +0200

    Javadoc article vendu

--- src/main/java/ArticleVendu.java
+++ src/main/java/ArticleVendu.java
@@ -1,5 +1,9 @@
-import java.sql.Date;
-
+/**
+ * La classe ArticleVendu représente un article vendu aux enchères.
+ * 
+ * @author [votre nom]
+ * @version 1.0
+ */
 public class ArticleVendu {
 	private int noArticle;
 	private String nomArticle;
@@ -11,7 +15,18 @@ public class ArticleVendu {
 	private Utilisateur utilisateur;
 	private Categorie categorie;
 	
-	
+	/**
+	 * Constructeur pour la classe ArticleVendu.
+	 * 
+	 * @param nomArticle le nom de l'article vendu.
+	 * @param description la description de l'article vendu.
+	 * @param dateDebutEncheres la date de début des enchères pour l'article.
+	 * @param dateFinEncheres la date de fin des enchères pour l'article.
+	 * @param prixInitial le prix initial de l'article vendu.
+	 * @param prixVente le prix final de l'article vendu.
+	 * @param utilisateur l'utilisateur ayant mis en vente l'article.
+	 * @param categorie la catégorie de l'article vendu.
+	 */
 	public ArticleVendu(String nomArticle, String description, Date dateDebutEncheres, Date dateFinEncheres,
 			int prixInitial, int prixVente, Utilisateur utilisateur, Categorie categorie) {
 		super();
@@ -25,11 +40,10 @@ public class ArticleVendu {
 		this.categorie = categorie;
 	}
 
-
+	/**
+	 * Constructeur par défaut pour la classe ArticleVendu.
+	 */
 	public ArticleVendu() {
 		super();
 	}
-	
-	
-	
 }
