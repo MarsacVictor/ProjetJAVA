3e2f7b2823de55031b083357eb1047190a6202c2
Victor MARSAC <victor.marsac2022@campus-eni.fr>
2023-04-06 10:29:06 +0200

    button

--- src/main/java/Servlet/ServletInscription.java
+++ src/main/java/Servlet/ServletInscription.java
@@ -18,7 +18,11 @@ import Class.Utilisateur;
 /**
  * Servlet implementation class ServletInscription
  */
-@WebServlet("/servletInscription")
+@WebServlet(
+		urlPatterns= {
+						"/servletCreer",
+						"/servletAnnuler"
+		})
 public class ServletInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +31,7 @@ public class ServletInscription extends HttpServlet {
      */
     public ServletInscription() {
         super();
-        // TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub      
     }
 
 	/**
@@ -35,6 +39,10 @@ public class ServletInscription extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub		
+		 if(request.getServletPath().equals("/servletAnnuler")) {
+	        	RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+				rd.forward(request, response);
+	     }
 	}
 
 	/**
@@ -42,50 +50,52 @@ public class ServletInscription extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		if(request.getParameter("mot_de_passe").equals(request.getParameter("confirmation"))) {
-			
-			HttpSession session = request.getSession();		
-			
-			String pseudo="";
-			String nom ="";
-			String prenom ="";
-			String email="";
-			String telephone ="";
-			String rue ="";
-			String code_postal ="";
-			String ville ="";
-			String mdp="";
-			int credit = 0;
-			
-			pseudo = request.getParameter("pseudo");
-			nom = request.getParameter("nom");
-			prenom = request.getParameter("prenom");
-			email = request.getParameter("email");
-			telephone = request.getParameter("telephone");
-			rue = request.getParameter("rue");
-			code_postal = request.getParameter("code_postal");
-			ville = request.getParameter("ville");
-			mdp = request.getParameter("mot_de_passe");
-			credit = 100;
-
-			
-			UtilisateurManager utilisateurManager = new UtilisateurManager();
-			utilisateurManager.AjouterUtilisateur(pseudo,nom, prenom, email,telephone, rue, code_postal, ville, mdp, credit);
-			
-			Utilisateur u = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mdp, credit, '0');
-			
-			session.setAttribute("utilisateur", u);
-			session.setMaxInactiveInterval(300);
-
-			request.setAttribute("utilisateur", u);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
-			rd.forward(request, response);
-		} else {
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/inscription.jsp");
-			System.err.println("Les deux mots de passe sont différent !");
-			rd.forward(request, response);
-		}	
+		if(request.getServletPath().equals("/servletCreer")) {
+			if(request.getParameter("mot_de_passe").equals(request.getParameter("confirmation"))) {
+				
+				HttpSession session = request.getSession();		
+				
+				String pseudo="";
+				String nom ="";
+				String prenom ="";
+				String email="";
+				String telephone ="";
+				String rue ="";
+				String code_postal ="";
+				String ville ="";
+				String mdp="";
+				int credit = 0;
+				
+				pseudo = request.getParameter("pseudo");
+				nom = request.getParameter("nom");
+				prenom = request.getParameter("prenom");
+				email = request.getParameter("email");
+				telephone = request.getParameter("telephone");
+				rue = request.getParameter("rue");
+				code_postal = request.getParameter("code_postal");
+				ville = request.getParameter("ville");
+				mdp = request.getParameter("mot_de_passe");
+				credit = 100;
+	
+				
+				UtilisateurManager utilisateurManager = new UtilisateurManager();
+				utilisateurManager.AjouterUtilisateur(pseudo,nom, prenom, email,telephone, rue, code_postal, ville, mdp, credit);
+				
+				Utilisateur u = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mdp, credit, '0');
+				
+				session.setAttribute("utilisateur", u);
+				session.setMaxInactiveInterval(300);
+	
+				request.setAttribute("utilisateur", u);
+				
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+				rd.forward(request, response);
+			} else {
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/inscription.jsp");
+				System.err.println("Les deux mots de passe sont différent !");
+				rd.forward(request, response);
+			}
+		}
 	}
 
 	
